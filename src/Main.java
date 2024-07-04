67cd2f3b24022dc829ee72c46a6630769ce10ace
Tharindu Senadeera <tharindusenadeera081@gmail.com>
2024-07-04 22:27:05 +0100

    BMI calculator added for testing

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Welcome to BMI Calculator!");
+
+        Scanner input  = new Scanner(System.in);
+
+        System.out.println("Enter your weight:");
+        double weight = input.nextDouble();
+
+        System.out.println("Enter your height in feet's:");
+        int heightInFeets = input.nextInt();
+
+        System.out.println("Enter remaining inches");
+        int heightInInches = input.nextInt();
+
+        int totalHeightInInches = (heightInFeets * 12) + heightInInches;
+
+        input.close();
+
+        double bmi = weight / Math.pow(totalHeightInInches, 2) * 703;
+
+        DecimalFormat df = new DecimalFormat("0.0");
+        System.out.println("Your BMI is:" + df.format(bmi));
     }
 }
